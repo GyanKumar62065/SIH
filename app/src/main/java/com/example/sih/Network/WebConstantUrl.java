751f19a18a4447704d4daa9d0fbc8828cb74ecf4
Gyan Kumar <gyankumar62065@gmail.com>
2022-11-24 23:27:25 +0530

    v.2.0.1 Final Application

--- app/src/main/java/com/example/sih/Network/WebConstantUrl.java
+++ app/src/main/java/com/example/sih/Network/WebConstantUrl.java
@@ -2,5 +2,5 @@ package com.example.sih.Network;
 
 public class WebConstantUrl {
 
-    public static String BASE_URL ="http://172.17.33.219:9090/" ;
+    public static String BASE_URL ="http://192.168.1.15:9090/" ;
 }
